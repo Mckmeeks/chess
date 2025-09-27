bcb0bd33084997648b2e825e2cf7d3305e202bb8
Mckmeeks <mckmeeks@byu.edu>
2025-09-27 12:41:50 -0600

    Passed ChessGameTests, ChessGame now returns a preset board.

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -21,7 +21,9 @@ public class ChessGame {
 
     public ChessGame() {
         setTeamTurn(TeamColor.WHITE);
-        setBoard(new ChessBoard());
+        ChessBoard tempBoard = new ChessBoard();
+        tempBoard.resetBoard();
+        setBoard(tempBoard);
     }
 
     /**
