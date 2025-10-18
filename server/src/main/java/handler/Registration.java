cfef0cdc38843a2957c3cd4f24215a3e02116189
Mckmeeks <mckmeeks@byu.edu>
2025-10-17 20:55:19 -0600

    Created first phase of Registration Handler

---
+++ server/src/main/java/handler/Registration.java
@@ -0,0 +1,32 @@
+package handler;
+
+import com.google.gson.Gson;
+
+import com.google.gson.JsonSyntaxException;
+import dataaccess.AlreadyTakenException;
+
+import handler.request.RegisterRequest;
+import service.result.RegisterResult;
+
+import dataaccess.interfaces.UserDAO;
+import dataaccess.interfaces.AuthDAO;
+
+import service.User;
+
+public class Registration {
+    private final AuthDAO aDAO;
+    private final UserDAO uDAO;
+    private final Gson serializer;
+
+    public Registration(UserDAO userDataAcc, AuthDAO authDataAcc) {
+        aDAO = authDataAcc;
+        uDAO = userDataAcc;
+        serializer = new Gson();
+    }
+
+    public RegisterResult run(String jsonRequest) throws JsonSyntaxException, AlreadyTakenException {
+        User userService = new User(uDAO, aDAO);
+        RegisterRequest request = serializer.fromJson(jsonRequest, RegisterRequest.class);
+        return userService.register(request);
+    }
+}
