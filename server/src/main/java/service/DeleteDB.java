59807c4192d7f57ccb95432dc0249df1b2a2b83f
Mckmeeks <mckmeeks@byu.edu>
2025-10-18 23:52:23 -0600

    Created database deletion service/handler

---
+++ server/src/main/java/service/DeleteDB.java
@@ -0,0 +1,23 @@
+package service;
+
+import dataaccess.interfaces.AuthDAO;
+import dataaccess.interfaces.GameDAO;
+import dataaccess.interfaces.UserDAO;
+
+public class DeleteDB {
+    final private UserDAO uDAO;
+    final private AuthDAO aDAO;
+    final private GameDAO gDAO;
+
+    public DeleteDB(UserDAO userDataAcc, AuthDAO authDataAcc, GameDAO gameDataAcc) {
+        uDAO = userDataAcc;
+        aDAO = authDataAcc;
+        gDAO = gameDataAcc;
+    }
+
+    public void clear() {
+        uDAO.clear();
+        aDAO.clear();
+        gDAO.clear();
+    }
+}
