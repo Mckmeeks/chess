031b06eb5b4e38d294e4c482f7cc22a9a7c01956
Mckmeeks <mckmeeks@byu.edu>
2025-10-18 23:49:17 -0600

    Enforced argument checking

--- server/src/main/java/handler/Registration.java
+++ server/src/main/java/handler/Registration.java
@@ -27,6 +27,8 @@ public class Registration {
     public String run(String jsonRequest) throws JsonSyntaxException, AlreadyTakenException {
         User userService = new User(uDAO, aDAO);
         RegisterRequest request = serializer.fromJson(jsonRequest, RegisterRequest.class);
+        if (request.username() == null) {throw new JsonSyntaxException("Error: Username not included");}
+        if (request.password() == null) {throw new JsonSyntaxException("Error: Password not included");}
         RegisterResult result = userService.register(request);
         return serializer.toJson(result);
     }
