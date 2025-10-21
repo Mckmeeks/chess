5fc7d4b938579e7ed37c2175b9d8969502b24cfa
Mckmeeks <mckmeeks@byu.edu>
2025-10-20 21:23:11 -0600

    Added additional unit tests

--- server/src/test/java/service/ServiceUnitTests.java
+++ server/src/test/java/service/ServiceUnitTests.java
@@ -1,8 +1,11 @@
 package service;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import io.javalin.http.BadRequestResponse;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import dataaccess.*;
 import dataaccess.interfaces.*;
@@ -10,7 +13,6 @@ import dataaccess.interfaces.*;
 import model.*;
 import handler.request.*;
 import service.result.*;
-import service.*;
 
 public class ServiceUnitTests {
     private static UserDAO uDAO;
@@ -24,27 +26,134 @@ public class ServiceUnitTests {
         gDAO = new MemoryGameDAO();
     }
 
-    static void clear() {
+    @BeforeEach
+    void clear() {
         uDAO.clear();
         aDAO.clear();
         gDAO.clear();
     }
 
-    @Test
-    void positiveRegister() throws AlreadyTakenException{
+    static RegisterResult register() throws AlreadyTakenException {
         var request = new RegisterRequest("TestUser", "pass","");
         User userService = new User(uDAO, aDAO);
-        var response = userService.register(request);
+        return userService.register(request);
+    }
+
+    @Test
+    void positiveRegister() throws AlreadyTakenException{
+        var response = register();
         assertEquals("TestUser", response.username());
         assertNotNull(response.authToken());
     }
 
     @Test
     void negativeRegisterTwice() throws AlreadyTakenException {
-        clear();
-        positiveRegister();
+        register();
         var request = new RegisterRequest("TestUser", "letMeIn","not.your@cheese.com");
         User userService = new User(uDAO, aDAO);
         assertThrows(DataAccessException.class, () -> userService.register(request));
+    }
+
+    @Test
+    void positiveLogin() throws InvalidAuthorizationException, AlreadyTakenException {
+        register();
+        var request = new LoginRequest("TestUser", "pass");
+        User userService = new User(uDAO, aDAO);
+        LoginResult result = userService.login(request);
+        assertNotNull(result.authToken());
+        assertNotNull(aDAO.getAuth(result.authToken()));
+    }
+
+    @Test
+    void negativeLoginInvalid() throws AlreadyTakenException {
+        var request = new LoginRequest("TestUser", "pass");
+        User userService = new User(uDAO, aDAO);
+        assertThrows(InvalidAuthorizationException.class, () -> userService.login(request));
+        register();
+        var invalidRequest = new LoginRequest("TestUser", "invalid");
+        assertThrows(InvalidAuthorizationException.class, () -> userService.login(invalidRequest));
+    }
+
+    @Test
+    void positiveLogout() throws InvalidAuthorizationException, AlreadyTakenException {
+        var registerResult = register();
+        assertNotNull(aDAO.getAuth(registerResult.authToken()));
+        User userService = new User(uDAO, aDAO);
+        userService.logout(registerResult.authToken());
+        assertNull(aDAO.getAuth(registerResult.authToken()));
+    }
+
+    @Test
+    void negativeLogout() {
+        User userService = new User(uDAO, aDAO);
+        assertThrows(InvalidAuthorizationException.class, () -> userService.logout("badAuthToken"));
+    }
+
+    @Test
+    void positiveNewGame() throws InvalidAuthorizationException, AlreadyTakenException {
+        var registerResult = register();
+        Game gameService = new Game(aDAO, gDAO);
+        CreateRequest request = new CreateRequest("testGame");
+        NewGameResult result = gameService.newGame(registerResult.authToken(), request);
+        assertNotNull(gDAO.getGame(result.gameID()));
+    }
+
+    @Test
+    void negativeNewGame() throws AlreadyTakenException {
+        var registerResult = register();
+        Game gameService = new Game(aDAO, gDAO);
+        CreateRequest request = new CreateRequest("");
+        assertThrows(BadRequestException.class, () -> gameService.newGame(registerResult.authToken(), request));
+    }
+
+    @Test
+    void positiveListGames() throws InvalidAuthorizationException, AlreadyTakenException {
+        var registerResult = register();
+        Game gameService = new Game(aDAO, gDAO);
+        CreateRequest request = new CreateRequest("testGame");
+        gameService.newGame(registerResult.authToken(), request);
+        ListResult result = gameService.listGames(registerResult.authToken());
+        assertNotNull(result);
+        assertEquals("testGame", result.getArray().getFirst().gameName());
+    }
+
+    @Test
+    void negativeListGames() {
+        Game gameService = new Game(aDAO, gDAO);
+        assertThrows(InvalidAuthorizationException.class, () -> gameService.listGames("badAuthToken"));
+    }
+
+    @Test
+    void positiveJoinResult() throws InvalidAuthorizationException, AlreadyTakenException, BadRequestResponse {
+        var registerResult = register();
+        Game gameService = new Game(aDAO, gDAO);
+        CreateRequest request = new CreateRequest("testGame");
+        gameService.newGame(registerResult.authToken(), request);
+
+        JoinRequest joinReq = new JoinRequest("WHITE", 0);
+        gameService.joinGame(registerResult.authToken(), joinReq);
+        var game = gDAO.getGame(joinReq.gameID());
+        assertEquals("TestUser", game.wUsername());
+    }
+
+    @Test
+    void negativeJoinResult() throws InvalidAuthorizationException, AlreadyTakenException {
+        var registerResult = register();
+        Game gameService = new Game(aDAO, gDAO);
+        CreateRequest request = new CreateRequest("testGame");
+        gameService.newGame(registerResult.authToken(), request);
+        gameService.listGames(registerResult.authToken());
+        JoinRequest joinReq = new JoinRequest("white", 0);
+        assertThrows(BadRequestException.class, () -> gameService.joinGame(registerResult.authToken(), joinReq));
+    }
+
+    @Test
+    void positiveClear() throws InvalidAuthorizationException, AlreadyTakenException {
+        positiveJoinResult();
+        DeleteDB dataService = new DeleteDB(uDAO, aDAO, gDAO);
+        dataService.clear();
+        assertEquals(0, aDAO.getSize());
+        assertEquals(0, uDAO.getSize());
+        assertEquals(0, gDAO.getSize());
     }
 }
