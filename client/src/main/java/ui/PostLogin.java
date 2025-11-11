acc6849afc84375cad41116af66adbce65ff4bd7
Mckmeeks <mckmeeks@byu.edu>
2025-11-11 15:10:46 -0700

    Implemented listGames and playGame, created helper testIdInput private method to verify user input

--- client/src/main/java/ui/PostLogin.java
+++ client/src/main/java/ui/PostLogin.java
@@ -1,12 +1,13 @@
 package ui;
 
 import exception.ResponseException;
+import model.GameData;
 import request.CreateRequest;
+import request.JoinRequest;
 import result.ListResult;
 import result.NewGameResult;
 import server.ServerFacade;
 
-import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Scanner;
 
@@ -49,7 +50,7 @@ public class PostLogin {
                 case "logout" -> logout();
                 case "create" -> createGame(prompt);
                 case "list" -> listGames();
-                case "join" -> playGame();
+                case "join" -> playGame(prompt);
                 case "observe" -> observeGame();
                 case "quit" -> quit();
                 default -> help();
@@ -99,20 +100,46 @@ public class PostLogin {
 
     private void listGames() throws ResponseException {
         ListResult result = server.listGames(authToken);
-
-        System.out.print(result.getArray());
+        wipeIdPairs();
+        System.out.println();
+        for (int i = 1; i <= result.getArray().size(); i++) {
+            GameData tempGame = result.getArray().get(i-1);
+            makeNewIdPair(i, tempGame.gameID());
+            System.out.println("\t" + i + ".\t" + tempGame.gameName() + "\tWhite: " + tempGame.whiteUsername() + "\t\tBlack: " + tempGame.blackUsername());
+        }
     }
 
-    private void playGame() {}
+    private void playGame(String[] prompt) throws ResponseException {
+        if (prompt.length != 3) {throw new IllegalArgumentException("Invalid arguments: join requires a game ID and a player COLOR");}
+        int id = testIdInput(prompt[1]);
+        if (!prompt[2].equals("WHITE") & !prompt[2].equals("BLACK")) {throw new IllegalArgumentException("Invalid arguments: join requires a WHITE or BLACK color description");}
+        server.joinGame(new JoinRequest(prompt[2], clientToGameIDs.get(id)), authToken);
+    }
 
-    private void observeGame() {}
+    private void observeGame(String[] prompt) {
+        int id = testIdInput(prompt[1]);
+
+    }
 
     private void printPrompt() {
         System.out.print("\n" + RESET_TEXT_COLOR + "[LOGGED_IN] >>> " + SET_TEXT_COLOR_BLUE);
     }
 
+    private void wipeIdPairs() {
+        clientToGameIDs = new Hashtable<>();
+        gameToClientIDs = new Hashtable<>();
+    }
+
     private void makeNewIdPair(Integer client, Integer game) {
         clientToGameIDs.put(client, game);
         gameToClientIDs.put(game, client);
+    }
+
+    private int testIdInput(String sd) {
+        int id;
+        try {id = Integer.parseInt(sd);}
+        catch (Exception ex) {throw new IllegalArgumentException("Invalid game ID");}
+        if (clientToGameIDs.get(id) == null) {throw new IllegalArgumentException("Invalid game ID");}
+        return id;
     }
 }
