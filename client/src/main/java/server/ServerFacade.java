368247664ae938276cf2e0548a398d1fd86d80d7
Mckmeeks <mckmeeks@byu.edu>
2025-11-10 20:10:13 -0700

    Implemented serverFacade public methods

--- client/src/main/java/server/ServerFacade.java
+++ client/src/main/java/server/ServerFacade.java
@@ -9,6 +9,9 @@ import java.net.http.*;
 
 import exception.ResponseException;
 
+import result.*;
+import request.*;
+
 public class ServerFacade {
     private final HttpClient client = HttpClient.newHttpClient();
     private final String serverUrl;
@@ -17,23 +20,62 @@ public class ServerFacade {
         serverUrl = url;
     }
 
-    public UserData register(UserData user) throws ResponseException {
-        HttpRequest request = buildRequest("POST", "/user", user);
+    public RegisterResult register(RegisterRequest reg) throws ResponseException {
+        HttpRequest request = buildRequest("POST", "/user", reg, null);
         HttpResponse<String> response = sendRequest(request);
-        return handleResponse(response, UserData.class);
+        return handleResponse(response, RegisterResult.class);
     }
 
-    private HttpRequest buildRequest(String method, String path, Object body) {
+    public LoginResult login(LoginRequest user) throws ResponseException {
+        HttpRequest request = buildRequest("POST", "/session", user, null);
+        HttpResponse<String> response = sendRequest(request);
+        return handleResponse(response, LoginResult.class);
+    }
+
+    public LogoutResult logout(String auth) throws ResponseException {
+        HttpRequest request = buildRequest("DELETE", "/session", null, auth);
+        HttpResponse<String> response = sendRequest(request);
+        return handleResponse(response, LogoutResult.class);
+    }
+
+    public NewGameResult createGame(CreateRequest game, String auth) throws ResponseException {
+        HttpRequest request = buildRequest("POST", "/game", game, auth);
+        HttpResponse<String> response = sendRequest(request);
+        return handleResponse(response, NewGameResult.class);
+    }
+
+    public ListResult listGames(String auth) throws ResponseException {
+        HttpRequest request = buildRequest("GET", "/game", null, auth);
+        HttpResponse<String> response = sendRequest(request);
+        return handleResponse(response, ListResult.class);
+    }
+
+    public JoinResult joinGame(JoinRequest join, String auth) throws ResponseException {
+        HttpRequest request = buildRequest("PUT", "/game", join, auth);
+        HttpResponse<String> response = sendRequest(request);
+        return handleResponse(response, JoinResult.class);
+    }
+
+    public void clear() throws ResponseException {
+        HttpRequest request = buildRequest("DELETE", "/db", null, null);
+        sendRequest(request);
+    }
+
+    private HttpRequest buildRequest(String method, String path, Object body, String header) {
         HttpRequest.Builder request = HttpRequest.newBuilder()
                 .uri(URI.create(serverUrl + path))
                 .method(method, makeRequestBody(body));
         if (body != null) {
             request.setHeader("Content-Type", "application/json");
         }
+        if (header != null) {
+            request.setHeader("Authorization", header);
+        }
         return request.build();
     }
 
     private HttpRequest.BodyPublisher makeRequestBody(Object body) {
+//        if (body instanceof String) { return HttpRequest.BodyPublishers.ofString((String)body); }
         if (body != null) { return HttpRequest.BodyPublishers.ofString(new Gson().toJson(body)); }
         else { return HttpRequest.BodyPublishers.noBody(); }
     }
@@ -51,7 +93,7 @@ public class ServerFacade {
         if (!isSuccessful(status)) {
             String body = response.body();
             if (body != null) {
-                throw ResponseException.fromJson(body);
+                throw ResponseException.fromJson(status, body);
             }
             throw new ResponseException(ResponseException.fromHttpStatusCode(status), "other failure: " + status);
         }
