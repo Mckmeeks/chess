9c0b281e12800135f3e435982f8a51c1e9ac1f98
Mckmeeks <mckmeeks@byu.edu>
2025-11-26 14:04:15 -0700

    Differentiated regular notifications verses move type notifications

--- shared/src/main/java/websocket/messages/Notification.java
+++ shared/src/main/java/websocket/messages/Notification.java
@@ -2,12 +2,24 @@ package websocket.messages;
 
 public class Notification extends ServerMessage {
     private final String message;
-    public Notification(String message) {
+    private final nType notificationType;
+
+    public enum nType {
+        SHALOM,
+        MOVE,
+    }
+
+    public Notification(String message, nType type) {
         super(ServerMessageType.NOTIFICATION);
         this.message = message;
+        this.notificationType = type;
     }
 
     public String getNotification() {
         return message;
+    }
+
+    public nType getType() {
+        return notificationType;
     }
 }
